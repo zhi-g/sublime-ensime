f9e5193f967d69622935bffc1863b63bf56384d7
Aemon Cannon <aemoncannon@gmail.com>
2012-07-11 18:42:19 -0400

    Add test of inserting into empty

--- diff.py
+++ diff.py
@@ -69,3 +69,4 @@ if __name__ == "__main__":
   assert len(diff("abc", "qqqqqbc")) == 1
   assert len(diff("abc", "")) == 1
   assert len(diff("abc\n   def", "abc   def")) == 1
+  assert len(diff("", "abcdef")) == 1
