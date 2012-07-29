66c4fc7210f0844e3db0d257eeb0cb889cf5e305
Brian Clapper <bmc@clapper.org>
2012-07-29 10:17:38 -0400

    Install script assumed ENSIME server tarball could be downloaded from this repo, but this repo has no downloads. Pointed URL to Aemon's repo, instead.

--- install-ensime-server.sh
+++ install-ensime-server.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading ensime server archive"
 ENSIME_VERSION=ensime_2.10.0-SNAPSHOT-0.9.6
-curl -OL# https://github.com/downloads/sublimescala/ensime/$ENSIME_VERSION.tar.gz
+curl -OL# https://github.com/downloads/aemoncannon/ensime/$ENSIME_VERSION.tar.gz
 echo "Extracting ensime server"
 tar xzf $ENSIME_VERSION.tar.gz
 mv $ENSIME_VERSION server
